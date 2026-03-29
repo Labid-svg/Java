931a52d66a3fecc905fd570bf273c52c565a0f3f
Nazmul Hassan <nazmulhassanlabid@gmail.com>
2026-03-29 20:35:53 +0600

    Add files via upload

---
+++ Practice_Problem_01/AreaOfTriangle2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle2 {
+        public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a: ");
+
+        double a = sc.nextDouble();
+
+        System.out.print("Enter b: ");
+
+        double b = sc.nextDouble();
+
+        System.out.print("Enter c: ");
+
+        double c = sc.nextDouble();
+
+        double s = (a+b+c)/3;
+
+        double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+        System.out.println("Area of Triangle: " + area);
+
+        sc.close();
+    }
+}
