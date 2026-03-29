2ad41f5636923409c677328d071da803d35ab93e
Nazmul Hassan <nazmulhassanlabid@gmail.com>
2026-03-29 20:14:23 +0600

    Add files via upload

---
+++ Practice_Problem_01/VolumeOfBall.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VolumeOfBall {
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+
+        double radius = sc.nextDouble();
+
+        double volume = (4.0/3.0) * Math.PI * Math.pow(radius,3);
+
+        System.out.print("Volume of Ball: " + volume);
+
+        sc.close();
+    }
+}
