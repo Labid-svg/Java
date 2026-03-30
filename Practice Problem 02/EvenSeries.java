40a0abb8e3208324a5e8f4917a4f4cda92dcc30b
Nazmul Hassan <nazmulhassanlabid@gmail.com>
2026-03-30 12:00:04 +0600

    Add files via upload

---
+++ Practice Problem 02/EvenSeries.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class EvenSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        //Using For Loop
+
+        // for(int i = 1; i<=num; i++){
+        //     if(i%2 == 0){
+        //         if(i == num){
+        //             System.out.println(i);
+        //             break;
+        //         }
+        //         System.out.print(i + ", ");
+        //     }
+        // }
+
+        // Using While Loop
+
+        // int i = 1;
+        // while(i<=num){
+        //     if(i%2 == 0){
+        //         if(i == num){
+        //             System.out.println(i);
+        //             break;
+        //         }
+        //         System.out.print(i + ", ");
+        //     }
+        //     i++;
+        // }
+
+        // Using Do-While Loop
+
+        // int i = 1;
+        // do{
+        //     if(i%2 == 0){
+        //         if(i == num){
+        //         System.out.println(i);
+        //         break;
+        //             }
+        //     System.out.print(i + ", ");
+        //     }
+        //     i++;
+        // } while(i<=num);
+
+        sc.close();
+    }
+}
